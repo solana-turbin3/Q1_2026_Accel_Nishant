4d4c89835b106e319c4380add4a68c0ccbe30681
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-02-15 18:37:59 +0530

    refactor: add Config struct and update state module in whitelist-transfer-hook program
    Introduced a new Config struct to manage admin and bump values, enhancing state management. Updated mod.rs to include the new config module and adjusted whitelist.rs to define WhitelistedUser with a user field, improving clarity and structure in the state definitions.

--- whitelist-transfer-hook/programs/whitelist-transfer-hook/src/state/config.rs
+++ whitelist-transfer-hook/programs/whitelist-transfer-hook/src/state/config.rs
@@ -1,11 +1,11 @@
 use anchor_lang::prelude::*;
 
 #[account]
-pub struct WhitelistedUser {
-    pub user: Pubkey,
+pub struct Config {
+    pub admin: Pubkey,
     pub bump: u8,
 }
 
-impl WhitelistedUser {
+impl Config {
     pub const LEN: usize = 8 + 32 + 1;
 }
--- whitelist-transfer-hook/programs/whitelist-transfer-hook/src/state/mod.rs
+++ whitelist-transfer-hook/programs/whitelist-transfer-hook/src/state/mod.rs
@@ -1,3 +1,5 @@
+pub mod config;
 pub mod whitelist;
 
+pub use config::*;
 pub use whitelist::*;
