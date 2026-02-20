cf35e933d9fa08f05d493a24e01304d046062773
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-02-20 16:42:20 +0530

    func: initialize lib with state

--- pinocchio-escrow/src/lib.rs
+++ pinocchio-escrow/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use pinocchio::{
+    entrypoint,
+    account_info::AccountInfo,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+pub mod state;
+pub mod instructions;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+entrypoint!(process_instruction);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+use pinocchio_pubkey::declare_id;
+declare_id!("4xMZozu4pZ1xEVW6giiRoRmtMWi1YGL8MsXBLaztwx5h");
+
+
+fn process_instruction(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> Result<(), ProgramError> {
+
+    let (ix_disc, _ix_data) = instruction_data
+    .split_first()
+    .ok_or(ProgramError::InvalidInstructionData)?;
+
+
+    match ix_disc {
+        0 =>  todo!(),
+        _ =>  Err(ProgramError::InvalidInstructionData)
     }
 }
